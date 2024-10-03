7e2e0fec4ffe49e24cf0d9f870fa5b75e6f9fd32
Jose <jleonardola@gmail.com>
2024-10-03 22:14:22 +0200

    s1

--- src/minishell/wildcard2.c
+++ src/minishell/wildcard2.c
@@ -6,7 +6,7 @@
 /*   By: jose-lop <jose-lop@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/19 14:51:08 by jose-lop      #+#    #+#                 */
-/*   Updated: 2024/09/20 13:44:28 by jose-lop      ########   odam.nl         */
+/*   Updated: 2024/10/03 22:13:25 by jose-lop      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,41 +64,35 @@ static int	split_len(char **s)
 	return (l);
 }
 
-bool	replace_and_insert(t_tkn_dlist *tmp, char **replaced)
+bool	replace_and_insert(t_tkn_dlist *to_replace, char **replaced)
 {
 	int			i;
-	t_tkn_dlist	*left;
-	t_tkn_dlist	*right;
 	t_tkn_dlist	*to_insert;
 	int			lstsize;
 
-	if (!tmp || !replaced)
+	if (!to_replace || !replaced)
 		return (false);
 	lstsize = split_len(replaced);
-	left = tmp;
-	right = tmp->next;
 	to_insert = empty_dlist(lstsize - 1);
-	if (left)
-		left->next = to_insert;
-	free(tmp->contents);
-	tmp->contents = ft_strdup(replaced[0]);
-	if (!tmp->contents)
+	to_replace->next = to_insert;
+	free(to_replace->contents);
+	to_replace->contents = ft_strdup(replaced[0]);
+	if (!to_replace->contents)
 		return (false);
 	if (lstsize == 1)
 		return (true);
 	i = 1;
+	//to_insert->prev = to_replace->next;
 	while (to_insert != NULL)
 	{
-		if (i == 0)
-			to_insert->prev = left;
 		if (i == lstsize)
-			to_insert->next = right;
-		if (!copy_nodevals(replaced[i], tmp, to_insert))
+			to_insert->next = to_replace->next;
+		if (!copy_nodevals(replaced[i], to_replace, to_insert))
 			return (false);
 		to_insert = to_insert->next;
 		i++;
 	}
-	if (right && right->next)
-		right->next->prev = to_insert;
+	if (to_replace->next && to_replace->next->next)
+		to_replace->next->next->prev = to_insert;
 	return (true);
 }
