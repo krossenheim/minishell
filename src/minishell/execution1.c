bf1e0ad40f3eac05743c1ad0f470b1d687bd31b9
Jose <jleonardola@gmail.com>
2024-10-08 21:08:02 +0200

    Norm exec 1

--- src/minishell/execution1.c
+++ src/minishell/execution1.c
@@ -6,47 +6,46 @@
 /*   By: jose-lop <jose-lop@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/03 12:42:53 by jose-lop      #+#    #+#                 */
-/*   Updated: 2024/10/08 16:50:10 by diwang        ########   odam.nl         */
+/*   Updated: 2024/10/08 21:07:22 by jose-lop      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
 int	is_builtin(t_hell *cur)
 {
-	if (ft_strncmp(cur->args[0], "env", 3) == 0 && ft_strlen(cur->args[0]) == 3)
+	if (ft_strncmp(cur->args[0], "env", 30) == 0)
 		return (1);
-	if (ft_strncmp(cur->args[0], "unset", 5) == 0 && ft_strlen(cur->args[0]) == 5)
+	if (ft_strncmp(cur->args[0], "unset", 50) == 0)
 		return (1);
-	if (ft_strncmp(cur->args[0], "pwd", 3) == 0 && ft_strlen(cur->args[0]) == 3)
+	if (ft_strncmp(cur->args[0], "pwd", 30) == 0)
 		return (1);
-	if (ft_strncmp(cur->args[0], "echo", 4) == 0 && ft_strlen(cur->args[0]) == 4)
+	if (ft_strncmp(cur->args[0], "echo", 40) == 0)
 		return (1);
-	if (ft_strncmp(cur->args[0], "exit", 4) == 0 && ft_strlen(cur->args[0]) == 4)
+	if (ft_strncmp(cur->args[0], "exit", 40) == 0)
 		return (1);
-	if (ft_strncmp(cur->args[0], "export", 6) == 0 && ft_strlen(cur->args[0]) == 6)
+	if (ft_strncmp(cur->args[0], "export", 60) == 0)
 		return (1);
-	if (ft_strncmp(cur->args[0], "cd", 2) == 0 && ft_strlen(cur->args[0]) == 2)
+	if (ft_strncmp(cur->args[0], "cd", 20) == 0)
 		return (1);
 	return (0);
 }
 
 int	exec_builtin(t_hell *head, t_mini *mini)
 {
-	if (ft_strncmp(head->args[0], "env", 3) == 0 && ft_strlen(head->args[0]) == 3)
+	if (ft_strncmp(head->args[0], "env", 30) == 0)
 		return (program_env(mini));
-	else if (ft_strncmp(head->args[0], "unset", 5) == 0 && ft_strlen(head->args[0]) == 5)
+	else if (ft_strncmp(head->args[0], "unset", 50) == 0)
 		return (program_unset(head, mini));
-	else if (ft_strncmp(head->args[0], "pwd", 3) == 0 && ft_strlen(head->args[0]) == 3)
+	else if (ft_strncmp(head->args[0], "pwd", 30) == 0)
 		return (program_pwd());
-	else if (ft_strncmp(head->args[0], "echo", 4) == 0 && ft_strlen(head->args[0]) == 4)
+	else if (ft_strncmp(head->args[0], "echo", 40) == 0)
 		return (program_echo(head));
-	else if (ft_strncmp(head->args[0], "exit", 4) == 0 && ft_strlen(head->args[0]) == 4)
+	else if (ft_strncmp(head->args[0], "exit", 40) == 0)
 		return (program_exit(*mini));
-	else if (ft_strncmp(head->args[0], "export", 6) == 0 && ft_strlen(head->args[0]) == 6)
+	else if (ft_strncmp(head->args[0], "export", 60) == 0)
 		return (program_export(head->args, mini));
-	else if (ft_strncmp(head->args[0], "cd", 2) == 0 && ft_strlen(head->args[0]) == 2)
+	else if (ft_strncmp(head->args[0], "cd", 20) == 0)
 		return (program_cd(mini));
 	return (1);
 }
