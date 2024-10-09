7b0c9fa12865bb034e6533b19463b04b0860eb27
Jose Leonardo Lopez Antequera <jose-lop@f1r1s1.codam.nl>
2024-10-09 13:02:55 +0200

    fix

--- src/minishell/execution.c
+++ src/minishell/execution.c
@@ -1,208 +1,228 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        ::::::::            */
-/*   execution.c                                        :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: jose-lop <jose-lop@student.codam.nl>         +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2024/09/03 12:42:53 by jose-lop      #+#    #+#                 */
-/*   Updated: 2024/10/08 22:15:42 by jose-lop      ########   odam.nl         */
-/*                                                                            */
+/*																			*/
+/*														::::::::			*/
+/*   execution.c										:+:	:+:			*/
+/*													 +:+					*/
+/*   By: jose-lop <jose-lop@student.codam.nl>		 +#+					 */
+/*												   +#+					  */
+/*   Created: 2024/09/03 12:42:53 by jose-lop	  #+#	#+#				 */
+/*   Updated: 2024/10/09 12:41:03 by jose-lop	  ########   odam.nl		 */
+/*																			*/
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	set_full_path(t_hell *head, t_mini *mini)
+void	_set_full_path(char **split_mypaths, t_hell *cur)
 {
 	int		i;
-	char	*find_path;
 	char	*confirm_path;
-	char	**split_mypaths;
+	char	*find_path;
 
-	head->path = NULL;
-	if (is_builtin(head))
-	{
-		head->path = ft_strdup(head->args[0]);
-		return ;
-	}
-	split_mypaths = ft_split(get_env_var("PATH", *mini), ':');
 	i = 0;
 	while (split_mypaths && split_mypaths[i] != NULL)
 	{
 		find_path = ft_strjoin(split_mypaths[i], "/");
-		confirm_path = ft_strjoin(find_path, head->args[0]);
+		confirm_path = ft_strjoin(find_path, cur->args[0]);
 		free(find_path);
-		if (head->path == NULL && access(confirm_path, F_OK) == 0)
+		if (cur->path == NULL && access(confirm_path, F_OK) == 0)
 		{
-			head->path = confirm_path;
+			cur->path = confirm_path;
 			break ;
 		}
 		else
 			free(confirm_path);
 		i++;
 	}
+}
+
+void	set_full_path(t_hell *head, t_mini *mini)
+{
+	char	**split_mypaths;
+
+	if (is_builtin(head))
+	{
+		head->path = ft_strdup(head->args[0]);
+		return ;
+	}
+	split_mypaths = ft_split(get_env_var("PATH", *mini), ':');
+	_set_full_path(split_mypaths, head);
 	free_split(split_mypaths);
 	if (!head->path && access(head->args[0], F_OK) == 0)
 		head->path = ft_strdup(head->args[0]);
-
 }
 
-static void 	ft_redirecs(t_hell  *head)
+static void	ft_redirecs(t_hell *head)
 {
 	if (head->infile > 0)
 	{
 		if (dup2(head->infile, STDIN_FILENO) == -1)
 			perror("Dup2 error infile");
 	}
-	if (head->outfile > 0) 
+	if (head->outfile > 0)
 	{
 		if (dup2(head->outfile, STDOUT_FILENO) == -1)
 			perror("Dup2 error outfile");
 	}
 }
 
-static void	ft_pipe(t_hell  *head, t_mini *mini, int fd[2])
+static void	ft_pipe(t_hell *head, t_mini *mini, int fd[2])
 {
 	if (head->next != NULL)
 	{
-		mini->to_exec->pipe = 1;	
+		mini->to_exec->pipe = 1;
 		if (pipe(fd) == -1)
 			perror("pipe");
 	}
 }
 
-static void ft_bite_size_write(int fd[2])
+static void	ft_bite_size_write(int fd[2])
 {
 	close(fd[0]);
 	if (dup2(fd[1], STDOUT_FILENO) == -1)
-			perror("dup2 fd[1]");
-	close(fd[1]); 
+		perror("dup2 fd[1]");
+	close(fd[1]);
 }
 
-static	int	ft_is_not_builtin(t_hell  *head, t_mini *mini, int fd[2], int *prev_fd)
+static	void	ft_is_not_builtin_child(int *prev_fd,
+	t_hell *head,
+	int fd[2],
+	t_mini *mini)
 {
-	pid_t pid;
-	char **envp;
-	
+	char	**envp;
+
+	if (*prev_fd != -1)
+	{
+		if (dup2(*prev_fd, STDIN_FILENO) == -1)
+			perror("dup2 prev_fd");
+		close(*prev_fd);
+	}
+	if (head->next != NULL)
+		ft_bite_size_write(fd);
+	ft_redirecs(head);
+	envp = envp_get(mini);
+	if (execve(head->path, head->args, envp) == 1)
+		perror("child execve");
+	free_split(envp);
+	exit(1);
+}
+
+static	int	ft_is_not_builtin(t_hell *head,
+	t_mini *mini,
+	int fd[2],
+	int *prev_fd)
+{
+	pid_t	pid;
+
 	ft_pipe(head, mini, fd);
 	pid = fork();
 	if (pid == -1)
 		perror("fork");
 	if (pid == -1)
 		return (-1);
-	if (pid == 0) 
-	{
-		if (*prev_fd != -1)
-		{
-			if (dup2(*prev_fd, STDIN_FILENO) == -1)
-				perror("dup2 prev_fd");
-			close(*prev_fd); 
-		}
-		if (head->next != NULL)
-			ft_bite_size_write(fd);
-		ft_redirecs(head);
-		envp = envp_get(mini);
-		if (execve(head->path, head->args, envp) == 1)
-			perror("child execve");
-		free_split(envp);
-		exit(1);
-	}
+	if (pid == 0)
+		ft_is_not_builtin_child(prev_fd, head, fd, mini);
 	mini->last_pid = pid;
 	return (pid);
 }
 
-static	int	ft_is_builtin_new(t_hell  *head, t_mini *mini, int fd[2], int *prev_fd)
+static	int	ftis(t_hell *head,
+	t_mini *mini,
+	int fd[2],
+	int *prev_fd)
 {
-	pid_t pid;
-	
+	pid_t	pid;
+
 	ft_pipe(head, mini, fd);
 	pid = fork();
 	if (pid == -1)
 		perror("fork");
 	if (pid == -1)
 		return (-1);
-	if (pid == 0) 
+	if (pid == 0)
 	{
 		if (*prev_fd != -1)
 		{
 			if (dup2(*prev_fd, STDIN_FILENO) == -1)
 				perror("dup2 prev_fd");
-			close(*prev_fd); 
+			close(*prev_fd);
 		}
 		if (head->next != NULL)
 			ft_bite_size_write(fd);
 		ft_redirecs(head);
 		exit(exec_builtin(head, mini));
-	}	
+	}
 	mini->last_pid = pid;
 	return (pid);
 }
 
-static void ft_set_in_out(t_mini *mini)
+static void	ft_set_in_out(t_mini *mini)
 {
 	mini->saved_stdout = dup(STDOUT_FILENO);
-	mini->saved_stdin = dup(STDIN_FILENO); 
+	mini->saved_stdin = dup(STDIN_FILENO);
 }
 
-static void ft_close_in_out(t_mini *mini)
+static void	ft_close_in_out(t_mini *mini)
 {
 	close(mini->saved_stdout);
-    close(mini->saved_stdin);
+	close(mini->saved_stdin);
 }
 
-static void	ft_close_redirecs(t_hell  *head)
+static void	ft_close_redirecs(t_hell *head)
 {
 	if (head->infile > 0)
 		close(head->infile);
-
 	if (head->outfile > 0)
 		close(head->outfile);
 }
 
-static void ft_parent(t_hell *head, int fd[2], int *prev_fd)
+static void	ft_parent(t_hell *head, int fd[2], int *prev_fd)
 {
 	if (*prev_fd != -1)
 		close(*prev_fd);
 	if (head->next != NULL)
 	{
-		close(fd[1]);  
-		*prev_fd = fd[0];  
+		close(fd[1]);
+		*prev_fd = fd[0];
 	}
 }
 
-int execution(t_mini *mini)
+static void	_execution(t_hell *h, t_mini *mini, int fd[2], int *prev_fd)
 {
-    t_hell  *head;
-    int     fd[2];        
-    int     prev_fd;
+	if (!h->path)
+	{
+		printf("Command '%s' not found.\n", h->args[0]);
+		mini->last_pid = -1;
+		mini->last_exit_code = 127;
+	}
+	else if (!mini->to_exec->next && is_builtin(h) == 1)
+	{
+		ft_redirecs(h);
+		mini->last_exit_code = exec_builtin(h, mini);
+		mini->last_pid = -1;
+		dup2(mini->saved_stdin, STDIN_FILENO);
+		dup2(mini->saved_stdout, STDOUT_FILENO);
+	}
+	else if (is_builtin(h) == 1 && ftis(h, mini, fd, prev_fd) != 0)
+		ft_parent(h, fd, prev_fd);
+	else if (ft_is_not_builtin(h, mini, fd, prev_fd) != 0)
+		ft_parent(h, fd, prev_fd);
+	ft_close_redirecs(h);
+}
+
+int	execution(t_mini *mini)
+{
+	t_hell	*h;
+	int		fd[2];
+	int		prev_fd;
 
 	prev_fd = -1;
 	ft_set_in_out(mini);
-    head = mini->to_exec;  
-    while (head != NULL)
-    {
-        if (!head->path)
-		{
-			printf("Command '%s' not found.\n", head->args[0]);
-			mini->last_pid = -1;
-			mini->last_exit_code = 127;
-		}
-		else if (!mini->to_exec->next && is_builtin(head) == 1)
-		{
-			ft_redirecs(head);
-			mini->last_exit_code = exec_builtin(head, mini);
-			mini->last_pid = -1;
-			dup2(mini->saved_stdin, STDIN_FILENO);
-			dup2(mini->saved_stdout, STDOUT_FILENO);
-
-		}
-		else if (is_builtin(head) == 1 && ft_is_builtin_new(head, mini, fd, &prev_fd) != 0)
-			ft_parent(head, fd, &prev_fd);
-		else if (ft_is_not_builtin(head, mini, fd, &prev_fd) != 0)
-			ft_parent(head, fd, &prev_fd);
-		ft_close_redirecs(head);
-        head = head->next;
-    }
+	h = mini->to_exec;
+	while (h != NULL)
+	{
+		_execution(h, mini, fd, &prev_fd);
+		h = h->next;
+	}
 	ft_close_in_out(mini);
 	if (mini->last_pid >= 0)
 	{
