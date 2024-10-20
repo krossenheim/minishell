93b98d74affc88420cbb00a3e615c707cfef68be
Jose <jleonardola@gmail.com>
2024-10-20 12:58:05 +0200

    Norminette/fix set_infile && add tester_errors

--- src/minishell/set_infile_n_outfile.c
+++ src/minishell/set_infile_n_outfile.c
@@ -6,7 +6,7 @@
 /*   By: jose-lop <jose-lop@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/08 21:49:03 by jose-lop      #+#    #+#                 */
-/*   Updated: 2024/10/20 12:45:09 by jose-lop      ########   odam.nl         */
+/*   Updated: 2024/10/20 12:48:49 by jose-lop      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,8 @@ bool	set_infile(t_hell *dest, t_tkn_dlist *current)
 		return (true);
 	if (ns->next && *ns->contents == '<' && ft_strlen(ns->contents) == 1)
 	{
+		if (!is_regular_file(ns->next->contents, true))
+			return (true);
 		maybe_close(dest->outfile);
 		file = open(get_filename(current)->contents, O_RDONLY, 0644);
 	}
@@ -64,8 +66,6 @@ bool	set_outfile(t_hell *dest, t_tkn_dlist *current)
 
 	ns = get_sep_r(current);
 	if (!ns || *ns->contents != '>')
-		return (true);
-	if (!is_regular_file(ns->next->contents, true))
 		return (true);
 	file = -2;
 	if (ft_strncmp(ns->contents, ">>", 2) == 0 && ft_strlen(ns->contents) == 2)
